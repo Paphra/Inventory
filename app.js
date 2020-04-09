386240ea0bf9620246d3d243c0674f29b721972f
Epaphradito Lugayavu <paphra.me@gmail.com>
2020-04-09 16:55:35 +0300

    Changed the database URI

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ var secret = Math.random() + '' + Math.random() + '' + Math.random();
 var upload = multer();
 
 /* Database connection [Mongoose] */
-var mongoDB = 'mongodb://localhost:27017/inventory?retryWrites=true?w=majority';
+var dev_db_url = "mongodb+srv://paphra:admin1@inventory@developmentcluster-4codo.mongodb.net/inventory?retryWrites=true&w=majority";
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
+
 mongoose.connect(mongoDB, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
